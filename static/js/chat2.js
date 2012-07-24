e14c414d17b6605806326ebf430142f3a3df92e2
sheimi <sheimi.zhang@gmail.com>
2012-07-25 00:28:24 +0800

    add a lot

--- static/js/chat2.js
+++ static/js/chat2.js
@@ -13375,42 +13375,6 @@ sheimi.chat = new Sheimi({
 }(sheimi, jQuery)
 //!function(sheimi, $) {
 
-var chatroom_frame = '<div id="chatrooms"><ul class="clearfix">'
-                   + '<li class="chatroom"><div class="chat-content"></div></li>'
-                   + '</ul><div class="chat-input">'
-                   + '<input placeholder="Your Message ..." id="chat-input" type="text"'
-                   + ' x-webkit-speech="" x-webkit-grammar="builtin:search" lang="en">'
-                   + '</div></div>'
-
-$(chatroom_frame).appendTo('body').css({'right': -500})
-var roomShowed = false
-function toggleChatRoom() {
-  roomShowed = !roomShowed
-  var change = roomShowed ? '+=500' : '-=500'
-  $('#chatrooms').animate({
-    'right': change
-  }, {
-    easing: 'easeInOutExpo'
-  })
-}
-var chatrooms_toggle = '<div id="chatrooms-toggle"><i class="icon-off icon-white"></i></div>'
-$(chatrooms_toggle).appendTo('body').click(function() {
-  toggleChatRoom()
-})
-
-$('#chat-input').keydown(function(e) {
-  if (e.keyCode != 13)
-    return
-  var msg = $(this).val()
-  var block = new MsgBlock({
-    type: 'msg'
-    , msg: msg
-    , user: 'I'
-  })
-  block.show()
-  $(this).val('')
-})
-
 /* ---- 
  * msg block
  * ----
@@ -13443,9 +13407,18 @@ function MsgBlock(options) {
   block.append(text)
   
   /* --- show the block --- */
-  function show() {
+  function show(chatroom) {
+    chatroom = chatroom == undefined ? sheimi.chat.currentChatRoom : chatroom
+
+    var last = $(chatroom + " .msg-block:last")
+
     var right = 500
-    block.appendTo(".chatroom .chat-content").css({
+    if (last.size() != 0) {
+      block.css({
+        'top': last.position().top + 41 
+      })
+    }
+    block.appendTo(chatroom + " .chat-content").css({
       'position': 'fixed'
       , 'right': -right
     }).animate({
@@ -13453,15 +13426,101 @@ function MsgBlock(options) {
     }, {
       easing: 'easeInOutExpo'
       , complete: function() {
+        last.css({
+          'margin-bottom': 0
+        })
         block.css({
           'position': 'static'
+          , 'margin-bottom': 40
         })
+        var height = $(chatroom + ' .chat-content').height() - $(chatroom).height()
+        $(chatroom).animate({scrollTop: height + 40})
       }
     })
+
+
   }
 
   /* --- interfaces --- */
   this.show = show
 }
+
+
+/* ---- 
+ * chat-room 
+ * ----
+ * options = {
+ *   type: str
+ *   , msg: str
+ *   , user: str(option)
+ * }
+ * ---- */
+function ChatRoom(options) {
+  defaultConfig = {
+    'name': '##Chatting Room##'
+  }
+  
+  config = sheimi.util.updateConfig(defaultConfig, options)
+  this.name = config.name
+  this.id = config.id
+  var chatroom = '<li id="chatroom-' + config.id 
+                          + '" class="chatroom" data-room-index="' + config.id 
+                          + '"><div class="chat-content"></div></li>'
+
+  function show() {
+    $("#chatrooms ul").append(chatroom)
+    active()
+  }
+
+  function active() {
+    sheimi.chat.currentChatRoom = '#chatroom-' + config.id
+  }
+
+  this.show = show
+  this.active = active
+}
+
+var chatroom_frame = '<div id="chatrooms"><ul class="clearfix">'
+//                   + '<li class="chatroom"><div class="chat-content"></div></li>'
+                   + '</ul><div class="chat-input">'
+                   + '<input placeholder="Your Message ..." id="chat-input" type="text"'
+                   + ' x-webkit-speech="" x-webkit-grammar="builtin:search" lang="en">'
+                   + '</div></div>'
+
+$(chatroom_frame).appendTo('body').css({'right': -500})
+
+var defaultRoom = new ChatRoom({
+  'name': 'BroadCast'
+  , 'id': 0
+})
+defaultRoom.show()
+
+var roomShowed = false
+function toggleChatRoom() {
+  roomShowed = !roomShowed
+  var change = roomShowed ? '+=500' : '-=500'
+  $('#chatrooms').animate({
+    'right': change
+  }, {
+    easing: 'easeInOutExpo'
+  })
+}
+var chatrooms_toggle = '<div id="chatrooms-toggle"><i class="icon-off icon-white"></i></div>'
+$(chatrooms_toggle).appendTo('body').click(function() {
+  toggleChatRoom()
+})
+
+$('#chat-input').keydown(function(e) {
+  if (e.keyCode != 13)
+    return
+  var msg = $(this).val()
+  var block = new MsgBlock({
+    type: 'msg'
+    , msg: msg
+    , user: 'I'
+  })
+  block.show()
+  $(this).val('')
+})
 
 //}(jQuery)
